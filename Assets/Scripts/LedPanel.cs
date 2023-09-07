6f9eeeebe3f715bc5aee48d785c61754f06a979d
Emile Birraux <112955984+Birox1er@users.noreply.github.com>
2023-09-07 21:54:36 +0200

    led panel

---
+++ Assets/Scripts/LedPanel.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LedPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private List<Image> list = new List<Image>();
+    int currentLed;
+
+    private void Start()
+    {
+        currentLed = 0;
+        int children = transform.childCount;
+        for (int i = 0; i < children; ++i)
+        {
+            if (transform.GetChild(i).GetComponent<Image>())
+                list.Add(transform.GetChild(i).GetComponent<Image>());
+        }
+    }
+    public void NewPeluche(int nbrPart)
+    {
+        currentLed = 0;
+        for(int i = 0;i<nbrPart; i++)
+        {
+            list[i].color = Color.white;
+            if (i == 1)
+            {
+                list[2].color = Color.black;
+            }
+        }
+        
+    }
+    public void ValidatePart(int nbrPart)
+    {
+        list[currentLed].color = Color.green;
+    }
+    public void FailedPart(int nbrPart)
+    {
+        list[currentLed].color = Color.red;
+    }
+}
--- Assets/Scripts/WorkZone.cs
+++ Assets/Scripts/WorkZone.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class WorkZone : MonoBehaviour
 {
     [SerializeField] private Spawner spawn;
+    [SerializeField] private LedPanel panel;
     private void OnTriggerEnter(Collider other)
     {
+        int a = other.transform.childCount;
         Debug.Log("is in work zone");
         spawn.Spawn();
+        panel.NewPeluche(a);
     }
     private void OnTriggerExit(Collider other)
     {
